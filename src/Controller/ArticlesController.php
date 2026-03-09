d2846dacdb7167aebe33d68a9957f6e008d5ce24
ines makhlouf <makhloufines73@gmail.com.com>
2026-03-09 21:48:26 +0000

    feat: Formulaires Symfony avec ArticleType, validation et messages flash

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -1,40 +1,62 @@
 <?php
-use App\Entity\Article;
-use Doctrine\ORM\EntityManagerInterface;
+// src/Controller/ArticlesController.php
+namespace App\Controller;
 
-// ... dans la classe ArticlesController
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+    use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
+class ArticlesController extends AbstractController
+{
+
 
 #[Route('/articles/nouveau', name: 'app_article_nouveau')]
-function nouveau(EntityManagerInterface $em): Response
+    public function nouveau(Request $request, EntityManagerInterface $em): Response
 {
     $article = new Article();
-    $article->setTitre('Mon premier article');
-    $article->setContenu('Ceci est le contenu de mon premier article créé avec Doctrine.');
-    $article->setAuteur('Étudiant');
-    $article->setDateCreation(new \DateTime());
-    $article->setPublie(true);
-
-    $em->persist($article);
-    $em->flush();
-
-    return new Response("Article créé avec l'id : " . $article->getId());
-}
-use App\Repository\ArticleRepository;
-
-
-#[Route('/articles', name: 'app_articles')]
-function index(ArticleRepository $articleRepository): Response
-{
-    $articles = $articleRepository->findAll();
-
-    return $this->render('articles/index.html.twig', [
-        'articles' => $articles,
+    
+    // Création du formulaire
+    $form = $this->createForm(ArticleType::class, $article);
+    
+    // Traitement de la requête
+    $form->handleRequest($request);
+    
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($article);
+        $em->flush();
+        
+        // Message flash de confirmation
+        $this->addFlash('success', 'Article créé avec succès !');
+        
+        return $this->redirectToRoute('app_articles');
+    }
+    
+    return $this->render('articles/nouveau.html.twig', [
+        'formulaire' => $form,
     ]);
 }
-#[Route('/articles/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
-function detail(Article $article): Response
-{
-    return $this->render('articles/detail.html.twig', [
-        'article' => $article,
-    ]);
+
+    // Route pour afficher tous les articles
+    #[Route('/articles', name: 'app_articles')]
+    public function index(ArticleRepository $articleRepository): Response
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    // Route pour afficher un article en détail
+    #[Route('/articles/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
+    public function detail(Article $article): Response
+    {
+        return $this->render('articles/detail.html.twig', [
+            'article' => $article,
+        ]);
+    }
 }
